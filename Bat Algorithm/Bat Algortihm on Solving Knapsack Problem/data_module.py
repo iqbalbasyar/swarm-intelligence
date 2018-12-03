b4dd3e96358a307f15e3fe92e0ed38c012136113
iqbalbasyar <the.underground.eleven@gmail.com>
2018-12-03 14:44:31 +0700

    Update data_module
    Change the specifications for data Row 0  is not datapoints. It was [total_data, maximum_weight]. So i took the max_w information from data_0.
    And add the max_w as the return values.
    
    The return was 4 : data_large_x, data_large_y, data_small_x, data_small_y.
    Now is 6 : data_large_x, data_large_y,data_large_maxw, data_small_x, data_small_y, data_small_maxw

--- Bat Algorithm/Bat Algortihm on Solving Knapsack Problem/data_module.py
+++ Bat Algorithm/Bat Algortihm on Solving Knapsack Problem/data_module.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[88]:
-
 
 import pandas as pd
 import numpy as np
@@ -21,22 +16,28 @@ def import_data():
     
     ls_X = []
     ls_Y = []
+    ls_W = []
     for filename in ls_filenames :
         #read data excluding last line (wich is the answer)
         x = pd.read_csv(ls_x_path+filename,names=['weight', 'price'], delimiter = ' ', header=None,error_bad_lines=False ) #you can use skipfooter=1 too 
-        ls_X.append(x)
+        max_w = x.iloc[0]['price']
+        ls_W.append(max_w)
+        ls_X.append(x.iloc[1:-1])
         y =  pd.read_csv(ls_y_path+filename,names=['optimal'], delimiter = ' ', header=None)
         ls_Y.append(y.iloc[0]['optimal'])
 
     ld_X = []
     ld_Y = []
+    ld_W = []
     for filename in ld_filenames :
         #read data excluding last line (wich is the answer)
         x = pd.read_csv(ld_x_path+filename,names=['weight', 'price'], delimiter = ' ', header=None,error_bad_lines=False ) #you can use skipfooter=1 too 
-        ld_X.append(x)
+        max_w = x.iloc[0]['price']
+        ld_W.append(max_w)
+        ld_X.append(x.iloc[1:])
         y =  pd.read_csv(ld_y_path+filename,names=['optimal'], delimiter = ' ', header=None)
         ld_Y.append(y.iloc[0]['optimal'])
         
     
-    return ls_X, ls_Y, ld_X, ld_Y
+    return ls_X, ls_Y, ls_W, ld_X, ld_Y, ld_W
 
